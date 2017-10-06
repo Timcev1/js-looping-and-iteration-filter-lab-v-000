66e6a14ec0da026fdbe9c071f23a6a085f4d1f58
Timothy Cevallos <timcev@gmail.com>
2017-10-06 01:46:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
-function findMatching(array, name){
-  return array.filter(function (drivername){
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
 }
